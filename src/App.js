894fc79157332259854bc6ce1357aadbac60ead7
Paul Szadurski <paulszadurski2@gmail.com>
2021-05-19 16:40:16 -0400

    added error handling and mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -10,18 +10,31 @@ function App() {
   const [text, setText] = useState('');
   const [score, setScore] = useState('');
 
+
   const handleClick = (event) => {
     event.preventDefault();
+
+    // if (title == '') {
+    //   alert('Please enter a title!')
+    // } else if (text == '') {
+    //   alert('Please write a review!')
+    // } 
 
     const movieRef = firebase.database().ref()
 
     const addReview = movieRef;
+
 
     addReview.push({
       title: title,
       text: text,
       score: score,
     })
+
+    setTitle('');
+    setText('');
+    setScore('');
+
   }
 
   // const handleReply = (event) => {
@@ -32,6 +45,7 @@ function App() {
   // }
 
   useEffect(() => {
+
     const movieRef = firebase.database().ref();
 
     movieRef.on('value', (response) => {
@@ -60,61 +74,51 @@ function App() {
 
   return (
     <div className="wrapper">
+        <nav>
+          <label for="about" tabIndex="0">About</label>
+          <input type="checkbox" name="about" id="about"></input>
+          <div className="about">
+            <h2>About MOOvies</h2>
+            <p>Hello there! Welcome to my anonymous movie review board made using react.js and firebase! The app is fairly straightforward, go to the review form and write up a review of a film you love (or hate) following the parameters. Once you submit the review, it will be sent to a firebase database and then pushed onto the review board you see beneath the form.</p>
+            <p>Click "About" again to remove this information.</p>
+          </div>
+        </nav>
       <header>
         <h1>Moovies 🐄 🐄 🐄</h1>
       </header>
       <main>
-        <form action="submit">
+        <form action="submit" onSubmit={handleClick}>
           <h2>Anonymous Review Form</h2>
           <label htmlFor="reviewTitle">Film Name: </label>
-          <input type="text" id="reviewTitle" onChange={(event) => setTitle(event.target.value)} value={title}></input>
+          <input required type="text" id="reviewTitle" onChange={(event) => setTitle(event.target.value)} value={title}></input>
 
           <label htmlFor="reviewText">Write Review Here:</label>
-          <textarea id="reviewText" onChange={(event) => setText(event.target.value)} value={text}></textarea>
+          <textarea required id="reviewText" onChange={(event) => setText(event.target.value)} value={text}></textarea>
 
           <label htmlFor="reviewScore">Score out of 10: </label>
-          <input type="number" id="reviewScore" max={10} min={1} onChange={(event) => setScore(event.target.value)} value={score}></input>
+          <input required type="number" id="reviewScore" max={10} min={1} onChange={(event) => setScore(event.target.value)} value={score} required></input>
 
-          <button onClick={handleClick}>Publish Review</button>
+          <button>Publish Review</button>
         </form>
         <ul className="listFlex">
           {movies.map((movie) => {
             return (
               
               <li key={movie.key}>
-                {/* {console.log(movie.key)} */}
                 <div>
                   <p className="title">{movie.name.title}</p>
-                  <button onClick={() => removeReview(movie.key)}>Remove Review</button>
+                  <button onClick={() => removeReview(movie.key)}>Remove Review </button>
                 </div>
                 <p className="text">{movie.name.text}</p>
                 <p className="score">{movie.name.score}/10</p>
-
-                {/* <form action="submit">
-                  <label htmlFor="reply">Write a Reply:</label>
-                  <textarea id={"reply" + movie.title}></textarea>
-
-                  <button onClick={(event) => {
-                    event.preventDefault();
-                    console.log(document.getElementById('reply' + movie.title).value);
-                    const replyValue = document.getElementById('reply' + movie.title).value;
-                    
-                    const node = document.createElement("li");
-                    const textnode = document.createTextNode(replyValue);
-                    console.log(textnode);
-                    node.appendChild(textnode);
-                    document.getElementById('replies' + movie.title).appendChild(node)
-                  } }>Submit Reply</button>
-                </form>
-
-                <ul id={'replies' + movie.title}>
-
-                </ul> */}
               </li>
             )
           })}
       </ul>
       </main>
+      <footer>
+        <p>Created by Paul Szadurski at Juno College</p>
+      </footer>
     </div>
   );
 }
