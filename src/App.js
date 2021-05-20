11e67b18e4730287a04dbd48da498220823ec854
Paul Szadurski <paulszadurski2@gmail.com>
2021-05-19 20:31:33 -0400

    added more functionality + google fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import firebase from './firebase';
+import { animateScroll as scroll } from 'react-scroll'
 
 function App() {
+
+
+  // fetch(url).then((response) => {
+  //   return response.json();
+  // }).then((jsonResponse) => {
+  //   console.log(jsonResponse.results[0].original_title);
+  // });
+
+
 
   const [movies, setMovies] = useState([]);
 
@@ -35,14 +45,9 @@ function App() {
     setText('');
     setScore('');
 
-  }
+    scroll.scrollToBottom();
 
-  // const handleReply = (event) => {
-  //   event.preventDefault();
-  //   return (
-  //     <p>hello there</p>
-  //   )
-  // }
+  }
 
   useEffect(() => {
 
@@ -70,10 +75,19 @@ function App() {
     movieRef.child(reviewID).remove();
   }
 
+  const returnToTop = () => {
+    scroll.scrollToTop();
+  }
+
   // console.log('movies: ', movies);
 
   return (
     <div className="wrapper">
+        {/* <div className="modal" id="modal">
+          <p>Are you sure you want to delete this?</p>
+        </div> */}
+
+
         <nav>
           <label htmlFor="about" tabIndex="0">About</label>
           <input type="checkbox" name="about" id="about"></input>
@@ -84,11 +98,11 @@ function App() {
           </div>
         </nav>
       <header>
-        <h1>Moovies 🐄 🐄 🐄</h1>
+        <h1>MOOvies 🐄 🐄 🐄</h1>
       </header>
       <main>
         <form action="submit" onSubmit={handleClick}>
-          <h2>Anonymous Review Form</h2>
+          <h2>Anonymous Movie Review Form</h2>
           <label htmlFor="reviewTitle">Film Name: </label>
           <input required type="text" id="reviewTitle" onChange={(event) => setTitle(event.target.value)} value={title}></input>
 
@@ -99,24 +113,30 @@ function App() {
           <input required type="number" id="reviewScore" max={10} min={1} onChange={(event) => setScore(event.target.value)} value={score} ></input>
           <button>Publish Review</button>
         </form>
+
+      <div className="reviewBoard">
+        <h2>Movie Review Board</h2>
         <ul className="listFlex">
           {movies.map((movie) => {
             return (
               
               <li key={movie.key}>
                 <div>
-                  <p className="title">{movie.name.title}</p>
-                  <button onClick={() => removeReview(movie.key)}>Remove Review </button>
+                  <h3 className="title">{movie.name.title}</h3>
+                  <button onClick={() => removeReview(movie.key)}>Remove</button>
                 </div>
                 <p className="text">{movie.name.text}</p>
                 <p className="score">{movie.name.score}/10</p>
               </li>
             )
           })}
-      </ul>
+        </ul>
+      </div>
+      <p onClick={returnToTop} className="return">Return to top</p>
       </main>
       <footer>
-        <p>Created by Paul Szadurski at Juno College</p>
+        <p>Created by Paul Szadurski at <a href="https://junocollege.com/">Juno College</a></p>
+        <p>Background image created by <a href="https://unsplash.com/@myke_simon">Myke Simon</a></p>
       </footer>
     </div>
   );
